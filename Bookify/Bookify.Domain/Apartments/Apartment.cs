01ac55706c94f9f05a3e2ff8041e6c0ec9c6e65a
Lara <larapetkovic021@gmail.com>
2024-02-11 19:25:03 +0100

    feat: Value objects added to Apartment entity

--- Bookify/Bookify.Domain/Apartments/Apartment.cs
+++ Bookify/Bookify.Domain/Apartments/Apartment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bookify.Domain.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +7,29 @@ using System.Threading.Tasks;
 
 namespace Bookify.Domain.Apartments
 {
-    public sealed class Apartment
+    public sealed class Apartment: Entity
     {
-        public Guid Id { get; private set; }
-        public string Name { get; private set; }
-        public string Description { get; private set; }
-        public string Country { get; private set; }
-        public string State { get; private set; }
-        public string ZipCode { get; private set; }
-        public string City { get; private set; }
-        public string Street { get; private set; }
-        public decimal PriceAmount { get; private set; }
-        public string PriceCurrency { get; private set; }
-        public decimal CleaningFeeAmount { get; private set; }
-        public string CleaningFeeCurrency { get; private set; }
+        public Apartment(Guid id,
+            Name name,
+            Description description,
+            Address address,
+            Money price,
+            Money cleaningFee,
+            List<Amenity> amenities): base(id)
+        {
+            Name = name;
+            Description = description;
+            Address = address;
+            Price = price;
+            CleaningFee = cleaningFee;
+            Amenities = amenities;
+        }
+        public Name Name { get; private set; }
+        public Description Description { get; private set; }
+        public Address Address { get; private set; }
+        public Money Price { get; private set; }
+        public Money CleaningFee { get; private set; }
         public DateTime? LastBookedOnUtc { get; private set; }
-        public Amenity Amenities { get; private set; } = new();
+        public List<Amenity> Amenities { get; private set; } = new();
     }
 }
