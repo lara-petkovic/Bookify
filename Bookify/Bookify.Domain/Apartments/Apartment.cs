5940ddc41a33aaa0f9036f94af693d996c21fb73
Lara <larapetkovic021@gmail.com>
2024-02-13 15:45:21 +0100

    feat: Booking events added

--- Bookify/Bookify.Domain/Apartments/Apartment.cs
+++ Bookify/Bookify.Domain/Apartments/Apartment.cs
@@ -1,9 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Bookify.Domain.Shared;
 
 namespace Bookify.Domain.Apartments
 {
--- Bookify/Bookify.Domain/Shared/Money.cs
+++ Bookify/Bookify.Domain/Shared/Money.cs
@@ -1,10 +1,12 @@
-﻿namespace Bookify.Domain.Apartments
+﻿using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Shared
 {
     public record Money(decimal Amount, Currency Currency)
     {
         public static Money operator +(Money first, Money second)
         {
-            if(first.Currency != second.Currency)
+            if (first.Currency != second.Currency)
             {
                 throw new InvalidOperationException("Currencies have to be equal"); ;
             }
@@ -12,5 +14,8 @@
         }
 
         public static Money Zero() => new(0, Currency.None);
+        public static Money Zero(Currency currency) => new(0, currency);
+
+        public bool IsZero() => this == Zero();
     }
 }
